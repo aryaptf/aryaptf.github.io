34927cb2b75de2e2c787fcee035ce92d5fc7b0c8
aryaptf <putraarya384@gmail.com>
2023-10-22 22:35:58 +0700

    update js

--- javascript.js
+++ javascript.js
@@ -4,4 +4,41 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    const nameInput = document.getElementById("nameInput");
+    const emailInput = document.getElementById("emailInput");
+    const subjectInput = document.getElementById("subjectInput");
+    const messageInput = document.getElementById("messageInput");
+    const submitButton = document.getElementById("submitButton");
+
+    submitButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        let errorMessage = "";
+        if (!nameInput.value) {
+            errorMessage += "Nama harus diisi.\n";
+        }
+        if (!emailInput.value) {
+            errorMessage += "Email harus diisi.\n";
+        } else {
+            const emailRegex = /@gmail\.com$/;
+            if (!emailRegex.test(emailInput.value)) {
+                errorMessage += "Alamat email harus mengandung '@gmail.com'.\n";
+            }
+        }
+        if (!subjectInput.value) {
+            errorMessage += "Subjek harus diisi.\n";
+        }
+        if (!messageInput.value) {
+            errorMessage += "Pesan harus diisi.\n";
+        }
+        if (errorMessage) {
+            alert(errorMessage);
+        } else {
+            alert("Pesan Anda terkirim!");
+            nameInput.value = "";
+            emailInput.value = "";
+            subjectInput.value = "";
+            messageInput.value = "";
+        }
+    });
 });
